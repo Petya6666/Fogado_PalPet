589ecc9d25c9d92a3b1ca57542caa08269ae7aa4
palpet478 <palpet478@hengersor.hu>
2025-10-06 12:19:05 +0200

    1. sql lekerdezes

--- Backend/server.js
+++ Backend/server.js
@@ -13,9 +13,20 @@ const db = mysql.createConnection({
     port: 3307
 });
 
+
+app.get("/fogado", (req, res) => {
+    const sql = "SELECT * FROM `szobak`";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})
+
+
 app.get('/ping', (req, res) => {
     res.json("Fut a backend");
     }   );
+
 
 app.listen(3001, () => {
     console.log("A szerver a  3001-es porton fut");
