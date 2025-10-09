a9312c2d3fa20dd8929c3bbf57642b4d7bde7c6e
palpet478 <palpet478@hengersor.hu>
2025-10-09 13:51:15 +0200

    valtozasok

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,52 +1,91 @@
-import React from 'react'
-import './fogado.css'
+import React, { useEffect, useState } from 'react';
+import './fogado.css';
+import axios from 'axios';
 
 function App() {
-  
+  const [szobak, setSzobak] = useState([]);
+  const [foglaltsag, setFoglaltsag] = useState([]);
+  const [kihasznaltsag, setKihasznaltsag] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/fogado').then(res => setSzobak(res.data));
+    axios.get('http://localhost:3001/foglaltsag').then(res => setFoglaltsag(res.data));
+    axios.get('http://localhost:3001/kihasznaltsag').then(res => setKihasznaltsag(res.data));
+  }, []);
 
   return (
     <>
-      <div>
-        <div></div>
+      <div className='kep'>
+        <img src="./img/top.jpg" alt="" />
       </div>
       <div>
-        <div>
-          <div>
-            <h3>Napraforgós Nemzeti Tanúsító Védjegy célja</h3>
-            A falusi szálláshelyek napraforgós Nemzeti Tanúsító Védjegye a FATOSZ által több mint tíz éve létrehozott, és működtetett minősítési rendszer alapjaira épülve 2011 óta a minőségi falusi turizmus szimbóluma. A védjegy alapvető célja, hogy – összhangban az egyes szálláshelyek működtetéséről szóló 239/2009. Korm. rendeletben foglaltakkal – garanciát nyújtson a szálláshely szolgáltatás minőségének megfelelő színvonalára. A falusi vendégházak 1-4 napraforgós besorolást nyerhetnek el a külső, belső megjelenés, a felszereltség, a szolgáltatások színvonala, valamint a szállásadó személyes felkészültségének, szakmai képzettségének függvényében.
-            <br />
-            <p></p>
-            <p></p>
-          </div>
-          <div>
-          <h3>Falusi szálláshely fajtái:</h3>
-
-      <li>Vendégszoba: a vendégek rendelkezésére bocsátható önálló lakóegység, amely egy lakóhelyiségből, és a minősítéstől függően a hozzátartozó mellékhelyiségekből áll.</li>
-      <li>Lakrész: önálló épület kettő, illetve több szobából álló lehatárolt része a minősítéstől függően hozzátartozó mellékhelyiségekkel együtt</li>
-      <li>Vendégház: önálló épület, több szobával, mellékhelyiségekkel és főzési lehetőséggel rendelkező lakó-, illetve üdülőegység, családok vagy kisebb csoportok elszállásolására.</li>
-      <li>Sátorozóhely: csak valamelyik falusi szálláshely típus mellett, mintegy azt kiegészítve üzemeltethető az előírt feltételek megléte esetén. Pl.: falusi vendégház sátorozóhellyel.</li>
-
-          </div>
-          <h3>A hét törpe fogadó</h3>
-
-      <li>Ruhásszekrény</li>
-      <li>Saját fürdőszoba zuhanytálca</li>
-      <li>WC (fürdőszobával egyben)</li>
+        <div className='bg-torzs'>
+          <h3>Napraforgós Nemzeti Tanúsító Védjegy célja</h3>
+          <p>
+            A falusi szálláshelyek napraforgós Nemzeti Tanúsító Védjegye a FATOSZ által több mint tíz éve létrehozott, és működtetett minősítési rendszer alapjaira épülve 2011 óta a minőségi falusi turizmus szimbóluma...
+          </p>
         </div>
-      </div>
-      <div>
-        <div>
-          <div>
-            <h3>A vendégszobák foglalatsága</h3>
-            <div></div>
+
+        <div className='feher'>
+          <h3>Falusi szálláshely fajtái:</h3>
+          <ul>
+            <li>Vendégszoba...</li>
+            <li>Lakrész...</li>
+            <li>Vendégház...</li>
+            <li>Sátorozóhely...</li>
+          </ul>
+            <img src="./img/ketagyas.jpg" />
           </div>
-          <div>
-            <h3>A szobák kihasználtsága:</h3>
-          </div>
+
+        <div className='bg-torzs'>
+          <h3>A hét törpe fogadó</h3>
+          <ul>
+            <li>Ruhásszekrény</li>
+            <li>Saját fürdőszoba zuhanytálca</li>
+            <li>WC (fürdőszobával egyben)</li>
+          </ul>
+        </div>
+
+        <div className='bg-torzs'>
+          <h3>Szobák listája</h3>
+          <ul>
+            {szobak.map(szoba => (
+              <li key={szoba.szoba}>
+                Szoba: {szoba.szoba}, Típus: {szoba.tipus}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className='bg-torzs'>
+          <h3>A vendégszobák foglaltsága</h3>
+          <ul>
+            {foglaltsag.map(f => (
+              <li key={f.név}>
+                Vendég: {f.név}, Érkezés: {f.érkezés}, Távozás: {f.távozás}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className='bg-torzs'>
+          <h3>A szobák kihasználtsága</h3>
+          <ul>
+            {kihasznaltsag.map(k => (
+              <li key={k.szoba}>
+                Szoba: {k.szoba}, Vendégek: {k.vendegek}, Vendégéjszakák: {k.vendegejszakak}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+  
+
+
+
