b11a14ff9a2e740937dabe96df6e6a94d3f48099
palpet478 <palpet478@hengersor.hu>
2025-10-14 13:24:51 +0200

    css tovabbfeljelsztese

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './fogado.css';
 import axios from 'axios';
 
+
 function App() {
   const [szobak, setSzobak] = useState([]);
   const [foglaltsag, setFoglaltsag] = useState([]);
@@ -18,18 +19,22 @@ function App() {
       <div className='kep'>
         <img src="./img/top.jpg" alt="" />
       </div>
-      <div>
+      <div className='container'>
         <div class='bg-torzs'>
           <h3>Napraforgós Nemzeti Tanúsító Védjegy célja</h3>
           <p>
-            A falusi szálláshelyek napraforgós Nemzeti Tanúsító Védjegye a FATOSZ által több mint tíz éve létrehozott, és működtetett minősítési rendszer alapjaira épülve 2011 óta a minőségi falusi turizmus szimbóluma...
+          A falusi szálláshelyek napraforgós Nemzeti Tanúsító Védjegye a FATOSZ által több mint tíz éve létrehozott, és működtetett minősítési rendszer alapjaira épülve 2011 óta a minőségi falusi turizmus szimbóluma. A védjegy alapvető célja, hogy – összhangban az egyes szálláshelyek működtetéséről szóló 239/2009. Korm. rendeletben foglaltakkal – garanciát nyújtson a szálláshely szolgáltatás minőségének megfelelő színvonalára. A falusi vendégházak 1-4 napraforgós besorolást nyerhetnek el a külső, belső megjelenés, a felszereltség, a szolgáltatások színvonala, valamint a szállásadó személyes felkészültségének, szakmai képzettségének függvényében.
           </p>
+          
           <a href="https://fatosz.hu/falusi-szallashely-minosites/">Tájékozató oldal</a>
-
+          <p></p>
           <img className='logo' src="./img/logo.png" alt="" />
+          <p></p>
+          <img src="./img/holloko_masolata.jpg" alt="" className='keret'/>
         </div>
-
-        <div class='bg-torzs'>
+        
+        <div class='bg-torzs' id='feher'>
+          
           <h3>Falusi szálláshely fajtái:</h3>
           <ul>
           <li>Vendégszoba: a vendégek rendelkezésére bocsátható önálló lakóegység, amely egy lakóhelyiségből, és a minősítéstől függően a hozzátartozó mellékhelyiségekből áll.</li>
@@ -37,9 +42,9 @@ function App() {
           <li>Vendégház: önálló épület, több szobával, mellékhelyiségekkel és főzési lehetőséggel rendelkező lakó-, illetve üdülőegység, családok vagy kisebb csoportok elszállásolására.</li>
           <li>Sátorozóhely: csak valamelyik falusi szálláshely típus mellett, mintegy azt kiegészítve üzemeltethető az előírt feltételek megléte esetén. Pl.: falusi vendégház sátorozóhellyel.</li>
           </ul>
-            <img src="./img/ketagyas.jpg" />
+            <img src="./img/ketagyas.jpg" className='keret' />
           </div>
-
+         
         <div class='bg-torzs'>
           
 <div className="contentbox" id="contbx3">
@@ -74,7 +79,7 @@ function App() {
 
         </div>
 
-       
+        </div>
         <div className=''>
           <h3>A vendégszobák foglaltsága</h3>
           <ul>
@@ -85,8 +90,8 @@ function App() {
             ))}
           </ul>
         </div>
-
-        <div className=''>
+      
+        <div className='kihasznaltsag'>
           <h3>A szobák kihasználtsága</h3>
           <ul>
             {kihasznaltsag.map(k => (
@@ -96,7 +101,7 @@ function App() {
             ))}
           </ul>
         </div>
-      </div>
+     
     </>
   );
 }
